54c908830e7995732ffac9489e606c93ee8fcf19
nivr <4402120+nivr@users.noreply.github.com>
2023-07-14 19:52:59 +0200

    finished fortune()

--- R/fortune.R
+++ R/fortune.R
@@ -1,12 +1,26 @@
-fortune <- function() {
-    files <- list.files(
+#' Unix fortune
+#'
+#' This function prints a random fortune from the fortunes package.
+#'
+#' @param include_obscene Whether to include obscene fortunes, TRUE or FALSE
+#' @return Invisible NULL
+#' @importFrom stringr str_replace_all
+#' @export
+fortune <- function(include_obscene = FALSE) {
+    if (!include_obscene) {
+        pattern <- "^[^-].*[^o]$"
+    } else {
+        pattern <- ".*"
+    }
+
+    fortune_files <- list.files(
         path = "inst",
-        pattern = "^[^-].*[^o]$",
+        pattern = pattern,
         recursive = FALSE,
         full.names = FALSE
     )
 
-    chosen_file <- sample(files, 1)
+    chosen_file <- sample(fortune_files, 1)
 
     fortunes <- scan(paste("inst", chosen_file, sep = "/"),
         what = "list",
@@ -17,9 +31,24 @@ fortune <- function() {
     )
 
     fortune_boundaries <- which(fortunes == "%")
-    chosen_boundary <- sample(seq_along(fortune_boundaries), 1)
+    chosen_boundary <- sample(seq_along(fortune_boundaries), 1) + 1
     fortune_boundaries <- c(1, fortune_boundaries)
     chosen_fortune <- fortunes[(fortune_boundaries[chosen_boundary - 1] + 1):
     (fortune_boundaries[chosen_boundary] - 1)]
+    if (endsWith(chosen_file, "-o")) {
+        chosen_fortune <- .replace_non_escaped(chosen_fortune)
+    }
     cat(paste0(chosen_fortune, collapse = "\n"))
+    return(invisible())
+}
+
+.rot13 <- function(x) {
+  chartr("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+         "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM",
+         x)
+}
+
+.replace_non_escaped <- function(string){
+  pattern <- "(?<!\\\\)[a-zA-Z]"
+  return(str_replace_all(string, pattern, function(x) .rot13(x)))
 }
